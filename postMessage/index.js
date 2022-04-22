1409fae6d71d80eeddc847239eec44cc1299a22d
drunk喵咪 <1684838553@qq.com>
2022-04-22 10:26:51 +0800

    Create index.js

---
+++ postMessage/index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+
+app.on('ready', function () {
+    // 创建窗口
+    let win = new BrowserWindow({
+        width: 1100,
+        height: 700,
+        // minWidth,minHeight,maxWidth,maxHeight
+        // x,y 窗口显示位置坐标
+        webPreferences: {
+            //  完整支持node 在html中引入node模块，不报错
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    })
+    // // 显示窗口控制台
+    // win.webContents.openDevTools()
+    win.loadFile('main.html')
+
+    // 关闭窗口，释放内存
+    win.on('close', () => {
+        win = null
+    })
+})
